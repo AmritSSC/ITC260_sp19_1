1d667dab8fd9f15d2a2f628416c860f4058f022b
AmritSSC <43659767+AmritSSC@users.noreply.github.com>
2019-04-30 11:59:38 -0700

    Added flickr pics and stuff

---
+++ sp19/application/application/config/custom_config.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//custom_config.php - a place to save global settings
+
+$config['style'] = 'amelia.css';
+$config['banner'] = 'Default Banner';
+$config['title'] = 'Default Title';
+$config['copyright'] = 'Default Copyright';
+$config['masthead'] = 'Default MastHead';
+$config['theme'] = 'themes/bootswatch/';
+$config['flickrKey'] = 'TEST';
+
+
+
+$config['nav1'] = array(
+    'news' => 'News',
+    'news/create' => 'Add News',
+    'pics' => 'Pics',
+);
